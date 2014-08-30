e698500c3abdab503d24da2d1fc8e846f4cf9c3b
Ryan Hiebert <ryan@ryanhiebert.com>
2014-08-29 22:00:03 -0500

    Make installable

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='undecorate',
+    version='0.1',
+    description='Let your decorations be undone',
+    long_description=open('README.rst').read(),
+    author='Ryan Hiebert',
+    author_email='ryan@ryanhiebert.com',
+    url='https://github.com/ryanhiebert/undecorate',
+    license='MIT License',
+    py_modules=['undecorate'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+    ]
+)
