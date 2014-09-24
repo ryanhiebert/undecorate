afafb47d77fd673abf8d8ce9baa9824b985a943a
Ryan Hiebert <ryan@ryanhiebert.com>
2014-09-24 11:47:26 -0500

    Add create_class_wrapper and class_wrapper
    Add two new functions that attempt to mirror the update_wrapper and
    wrapper functions from the functools module in the stdlib.
    
    The primary notable difference is that it works on classes instead of
    functions. This necessitates a couple of other differences. First, since
    __class__, __module__, and __name__ cannot be changed on a class, it
    cannot actually update the wrapper class, it must create a new classed
    based on it's template. Second, it uses class inheritance to mimic the
    unchanged behavior of the wrapped class rather than attempting to copy
    all of that functionality into a new class.
    
    class_wraps is then a decorator analagous to wraps. Both take keyword
    arguments 'deleted' and 'assigned', which default to
    CLASS_WRAPPER_DELETES and CLASS_WRAPPER_ASSIGNMENTS respectively.
    
    It ensures a __wrapped__ property on the new class that is analagous to
    the __wrapped__ that update_wrapper() puts on a wrapper function.
    
    This code has _absolutely no_ tests. Yet.

--- undecorate.py
+++ undecorate.py
@@ -29,7 +29,7 @@ TypeError: wrapper() got an unexpected keyword argument 'c'
 ('a', 'b', 'c', None)
 """
 
-from functools import wraps
+from functools import wraps, partial
 
 
 def unwrappable(decorator):
@@ -53,6 +53,57 @@ def unwrap(wrapped):
         if decoration is None:
             return wrapped
         wrapped = decoration
+
+
+CLASS_WRAPPER_DELETES = ('__dict__', '__doc__', '__weakref__')
+CLASS_WRAPPER_ASSIGNMENTS = ('__module__',)
+
+
+def create_class_wrapper(wrapper,
+                         wrapped,
+                         deleted=CLASS_WRAPPER_DELETES,
+                         assigned=CLASS_WRAPPER_ASSIGNMENTS):
+    """Create a wrapper class that looks like the wrapped class.
+
+    wrapper is the class used to override the wrapped class.
+    wrapped is the class has values overridden by the wrapper.
+    deleted is a tuple naming the __dict__ items to be removed from the
+    wrapper class (defaults to CLASS_WRAPPER_DELETES).
+    assigned is a tuple naming the __dict__ items to be copied directly
+    from the wrapped class (defaults to CLASS_WRAPPER_ASSIGNMENTS).
+
+    A notable difference from update_wrapper is that is creates a new class
+    that does not appear to be exactly the same as the wrapped class, but
+    rather mimics the name and the module, and inherits from the original
+    class, relying on class inheritance to mimic the behavior.
+    """
+    __dict__ = dict(wrapper.__dict__)
+
+    for attr in deleted:
+        __dict__.pop(attr)
+
+    for attr in assigned:
+        __dict__[attr] = getattr(wrapped, attr)
+
+    __dict__['__wrapped__'] = wrapped
+
+    # Use the metaclass of the wrapped class
+    return wrapped.__class__(wrapped.__name__, (wrapped,), __dict__)
+
+
+def class_wraps(wrapped,
+                deleted=CLASS_WRAPPER_DELETES,
+                assigned=CLASS_WRAPPER_ASSIGNMENTS):
+    """Decorator factory to apply create_class_wrapper() to a wrapper class.
+
+    Return a decorator that invokes create_class_wrapper() with the decorated
+    class as the wrapper argument and the arguments to class_wraps() as the
+    remaining arguments. Default arguments are as for create_class_wrapper().
+    This is a convenience function to simplify applying partial() to
+    create_class_wrapper().
+    """
+    return partial(create_class_wrapper, wrapped=wrapped,
+                   deleted=deleted, assigned=assigned)
 
 
 if __name__ == '__main__':
